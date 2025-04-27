e2a66d232d7a500b691bc2d6eb679a4512f37a3a
Sefovic <136874145+sefovic@users.noreply.github.com>
2025-04-27 15:53:21 +0300

    Add files via upload

---
+++ Kurulum-Setup/items.lua
@@ -0,0 +1,7 @@
+--burayı qb-core/shared/items.lua'ya yapıstırın.
+
+
+
+-- sfv-watermelon
+["karpuz"] = {["name"] = "karpuz", ["label"] = "Karpuz", ["weight"] = 200, ["type"] = "item", ["image"] = "karpuz.png", ["unique"] = false, ["useable"] = true, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Taptaze Dalından Karpuz", ['thirst'] = math.random(35, 54) ,['created'] = nil, ['decay'] = 14.0},
+["karpuzsuyu"] = {["name"] = "karpuzsuyu", ["label"] = "Karpuz Suyu", ["weight"] = 200, ["type"] = "item", ["image"] = "karpuzalt.png", ["unique"] = false, ["useable"] = true, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Taptaze Dalından Koparılmış Karpuzun Suyu", ['thirst'] = math.random(35, 54) ,['created'] = nil, ['decay'] = 14.0},

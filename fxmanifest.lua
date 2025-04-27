717995f6cac0d7887fed9b21e8a52d87404b35a6
Sefovic <136874145+sefovic@users.noreply.github.com>
2025-04-27 15:52:59 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'sfv-watermelon'
+
+client_script 'config.lua'
+client_script 'client.lua'
+
+server_script 'config.lua'
+server_script 'server.lua'
+
+dependencies {
+    'qb-core',
+    'ox-inventory',
+    'ox-target',
+    'ox_lib',
+    'progressbar',
+    'PolyZone'
+}

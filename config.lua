49bb42ca983ea1a0e3ca4119ec35a92d42712ed4
Sefovic <136874145+sefovic@users.noreply.github.com>
2025-04-27 15:52:03 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- Karpuz toplama noktaları (vector4: x, y, z, heading)
+Config.PickupLocations = {
+    vector4(2223.0, 5576.5, 53.8, 90.0),
+    vector4(2226.1, 5579.7, 53.8, 180.0),
+    vector4(2228.4, 5573.2, 53.8, 0.0)
+}
+
+-- NPC'ler
+Config.ProcessingNPC = vector4(2321.5, 2571.6, 46.7, 180.0)
+Config.SellNPC = vector4(1200.0, -1275.0, 35.0, 0.0)
